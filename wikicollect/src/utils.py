b9bf94050d88c9d1b3622a47bf91d40385a0fb44
pierre <pierre.pessarossi@gmail.com>
2023-10-31 18:42:11 +0100

    Add helpers to retrieve filename from path and check that path exists

--- wikicollect/src/utils.py
+++ wikicollect/src/utils.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 from pathlib import Path
 
@@ -16,3 +17,15 @@ def load_yaml(file_path: Path):
     with open(file_path, "r") as f:
         file = safe_load(f)
     return file
+
+
+def return_path_if_exists(path: Path) -> Path:
+    if path.exists():
+        return path
+    else:
+        raise ValueError(f"Path {path} does not exist.")
+
+
+def retrieve_filename_from_path(path: Path) -> str:
+    base_name, _ = os.path.splitext(path.name)
+    return base_name
